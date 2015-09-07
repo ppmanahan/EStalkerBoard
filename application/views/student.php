cbd15b35e8e185b11b259f06b1ab2a87964c2dfa
JolGspr <jolinagaspar@yahoo.com>
2015-09-07 08:06:51 +0800

    Added and implemented image in student page

--- application/views/student.php
+++ application/views/student.php
@@ -4,10 +4,17 @@
   <div class="main-wrap">
     <div class="page-header">
         <h5><?php echo $student['name'] ?></h5>
+        <?php echo '<span style="float: right;">' . $student['course'] . '</span>'?>
         <p><?php echo $sn ?></p>
     </div>
     <small>Schedule</small>
-    <img src="<?=base_url()?>public/img/icons/png/student.png" height="100" width="100" class="displayed" />
+    <?php if($student['image'] === NULL){
+     //do nothing
+    }else{
+       echo '<img src=' . base_url() . 'public/db_img/' . $student['image'] . ' height="300" width="1000" class="displayed" />';
+      echo '<br/>';
+    }
+    ?>
     <div class="result-wrap">
       <small>Total units: <b><?php echo $totalUnits['units'] ?></b></small>
       <hr style="height:1px; color:#999999; background-color:#999999;">
